873aa51c3ed7135c387e349225ac00de326c2e45
locutorgilfm-rgb <locutorgilfm@gmail.com>
2026-04-28 16:29:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -385,4 +385,5 @@ app.get("/render-status/:renderId", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Servidor na porta ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Servidor na porta ${PORT}`)); 
+ 
